592df1c20daa5af7399fcca902a3ab4d41b57521
bei <zhibei66@gmail.com>
2025-04-13 19:02:12 +0800

    更新插件版本号至 1.2.0，提升了版本管理的一致性。

--- main.py
+++ main.py
@@ -395,7 +395,7 @@ class VoiceConverter:
     name="so-vits-svc-api",
     author="Soulter",
     desc="So-Vits-SVC API 语音转换插件",
-    version="1.0.0",
+    version="1.2.0",
 )
 class SoVitsSvcPlugin(Star):
     """So-Vits-SVC API 插件主类"""
